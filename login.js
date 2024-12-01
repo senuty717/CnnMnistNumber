512d98c9bae0a33b452aaf12cf32e52a8d1885ec
senuty717 <79796532+senuty717@users.noreply.github.com>
2024-12-01 19:30:26 +0100

    Update login.js

--- login.js
+++ login.js
@@ -35,17 +35,6 @@ async function generarHash(contraseña) {
   return hashHex;
 }
 
-// Función para mostrar datos en la consola (para el rol admin)
-function mostrarDatosConsola(datos) {
-  const consola = document.getElementById('consola');
-  consola.innerHTML = ""; // Limpiar consola antes de agregar nuevos datos
-  datos.forEach(dato => {
-    const linea = document.createElement("p");
-    linea.textContent = dato; // Agregar cada línea de datos
-    consola.appendChild(linea);
-  });
-}
-
 // Función para mostrar el contenido basado en el rol
 function mostrarContenido(rol) {
   // Ocultar todo el contenido de roles
@@ -59,7 +48,7 @@ function mostrarContenido(rol) {
       adminContent.classList.remove("oculto");
       // Guardar el rol en localStorage
       localStorage.setItem("rol", "admin");
-      mostrarDatosConsola([ // Mostrar la consola después de iniciar sesión como admin
+      mostrarDatosConsola([  // Mostrar la consola después de iniciar sesión como admin
         "Modelo 1: Predicción correcta",
         "Modelo 2: Predicción incorrecta",
         "Modelo 3: Predicción correcta",
@@ -80,7 +69,7 @@ function mostrarContenido(rol) {
 
   // Eliminar la clase 'oculto' de todos los elementos con la clase 'contenido-roles'
   const contenidosRoles = document.querySelectorAll('.contenido-roles');
-  contenidosRoles.forEach(function (contenido) {
+  contenidosRoles.forEach(function(contenido) {
     contenido.classList.remove("oculto");
   });
 
@@ -92,7 +81,7 @@ function mostrarContenido(rol) {
 }
 
 // Función para manejar el evento de inicio de sesión
-form.addEventListener("submit", async function (event) {
+form.addEventListener("submit", async function(event) {
   event.preventDefault(); // Prevenir el envío del formulario
 
   const username = document.getElementById("username").value;
